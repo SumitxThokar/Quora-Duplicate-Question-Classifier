4a696f1dbbcbd3e627dd1adb136806aaae434af6
Sumit Thokar <110692286+SumitxThokar@users.noreply.github.com>
2023-05-30 21:57:25 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import helper
+import pickle
+import warnings
+model=pickle.load(open('model.pkl','rb'))
+
+st.header('Duplicate Quora Question Pairs')
+
+q1=st.text_input('Enter question 1')
+q2=st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query=helper.query_point_creator(q1,q2)
+    result=model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
---
+++ helper.py
@@ -0,0 +1,214 @@
+import re
+from bs4 import BeautifulSoup
+import distance
+import pickle
+import numpy as np
+
+cv=pickle.load(open('cv.pkl','rb'))
+
+def prepare(q):
+    # Lower case
+    q = str(q).lower().strip()
+    # Replacing Special character
+    q = q.replace('%', ' percent')
+    q = q.replace('$', ' dollar ')
+    q = q.replace('₹', ' rupee ')
+    q = q.replace('€', ' euro ')
+    q = q.replace('@', ' at ')
+    q = q.replace('[math]', '')
+    q = q.replace(',000,000,000 ', 'b ')
+    q = q.replace(',000,000 ', 'm ')
+    q = q.replace(',000 ', 'k ')
+    q = re.sub(r'([0-9]+)000000000', r'\1b', q)
+    q = re.sub(r'([0-9]+)000000', r'\1m', q)
+    q = re.sub(r'([0-9]+)000', r'\1k', q)
+    contractions = {
+        "ain't": "am not",
+        "aren't": "are not",
+        "can't": "can not",
+        "can't've": "can not have",
+        "'cause": "because",
+        "could've": "could have",
+        "couldn't": "could not",
+        "couldn't've": "could not have",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hadn't've": "had not have",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he'd": "he would",
+        "he'd've": "he would have",
+        "he'll": "he will",
+        "he'll've": "he will have",
+        "he's": "he is",
+        "how'd": "how did",
+        "how'd'y": "how do you",
+        "how'll": "how will",
+        "how's": "how is",
+        "i'd": "i would",
+        "i'd've": "i would have",
+        "i'll": "i will",
+        "i'll've": "i will have",
+        "i'm": "i am",
+        "i've": "i have",
+        "isn't": "is not",
+        "it'd": "it would",
+        "it'd've": "it would have",
+        "it'll": "it will",
+        "it'll've": "it will have",
+        "it's": "it is",
+        "let's": "let us",
+        "ma'am": "madam",
+        "mayn't": "may not",
+        "might've": "might have",
+        "mightn't": "might not",
+        "mightn't've": "might not have",
+        "must've": "must have",
+        "mustn't": "must not",
+        "mustn't've": "must not have",
+        "needn't": "need not",
+        "needn't've": "need not have",
+        "o'clock": "of the clock",
+        "oughtn't": "ought not",
+        "oughtn't've": "ought not have",
+        "shan't": "shall not",
+        "sha'n't": "shall not",
+        "shan't've": "shall not have",
+        "she'd": "she would",
+        "she'd've": "she would have",
+        "she'll": "she will",
+        "she'll've": "she will have",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "shouldn't've": "should not have",
+        "so've": "so have",
+        "so's": "so as",
+        "that'd": "that would",
+        "that'd've": "that would have",
+        "that's": "that is",
+        "there'd": "there would",
+        "there'd've": "there would have",
+        "there's": "there is",
+        "they'd": "they would",
+        "they'd've": "they would have",
+        "they'll": "they will",
+        "they'll've": "they will have",
+        "they're": "they are",
+        "they've": "they have",
+        "to've": "to have",
+        "wasn't": "was not",
+        "we'd": "we would",
+        "we'd've": "we would have",
+        "we'll": "we will",
+        "we'll've": "we will have",
+        "we're": "we are",
+        "we've": "we have",
+        "weren't": "were not",
+        "what'll": "what will",
+        "what'll've": "what will have",
+        "what're": "what are",
+        "what's": "what is",
+        "what've": "what have",
+        "when's": "when is",
+        "when've": "when have",
+        "where'd": "where did",
+        "where's": "where is",
+        "where've": "where have",
+        "who'll": "who will",
+        "who'll've": "who will have",
+        "who's": "who is",
+        "who've": "who have",
+        "why's": "why is",
+        "why've": "why have",
+        "will've": "will have",
+        "won't": "will not",
+        "won't've": "will not have",
+        "would've": "would have",
+        "wouldn't": "would not",
+        "wouldn't've": "would not have",
+        "y'all": "you all",
+        "y'all'd": "you all would",
+        "y'all'd've": "you all would have",
+        "y'all're": "you all are",
+        "y'all've": "you all have",
+        "you'd": "you would",
+        "you'd've": "you would have",
+        "you'll": "you will",
+        "you'll've": "you will have",
+        "you're": "you are",
+        "you've": "you have"
+    }
+    q_pre = []
+    for word in q.split():
+        if word in contractions:
+            word = contractions[word]
+        q_pre.append(word)
+        q = ' '.join(q_pre)
+
+    q = q.replace("'ve", " have")
+    q = q.replace("n't", " not")
+    q = q.replace("'re", " are")
+    q = q.replace("'ll", " will")
+
+    # Removing HTML tags
+    q = BeautifulSoup(q)
+    q = q.get_text()
+
+    # Remove punctuations
+    pattern = re.compile('\W')
+    q = re.sub(pattern, ' ', q).strip()
+
+    return q
+
+
+# Common words
+def common(x):
+    w1=set(map(lambda x:x.lower().strip(),x['question1'].split(" ")))
+    w2=set(map(lambda x:x.lower().strip(),x['question2'].split(" ")))
+    return len(w1 & w2) # word in both question
+
+# Total words.
+def total(x):
+    w1=set(map(lambda x:x.lower().strip(),x['question1'].split(" ")))
+    w2=set(map(lambda x:x.lower().strip(),x['question2'].split(" ")))
+    return (len(w1)+len(w2))
+
+
+def test_common_words(q1,q2):
+    w1 = set(map(lambda word: word.lower().strip(), q1.split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), q2.split(" ")))
+    return len(w1 & w2)
+
+def test_total_words(q1,q2):
+    w1 = set(map(lambda word: word.lower().strip(), q1.split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), q2.split(" ")))
+    return (len(w1) + len(w2))
+
+def query_point_creator(q1, q2):
+    input_query = []
+
+    # preprocess
+    q1 = prepare(q1)
+    q2 = prepare(q2)
+
+    # fetch basic features
+    input_query.append(len(q1))
+    input_query.append(len(q2))
+
+    input_query.append(len(q1.split(" ")))
+    input_query.append(len(q2.split(" ")))
+
+    input_query.append(test_common_words(q1, q2))
+    input_query.append(test_total_words(q1, q2))
+    input_query.append(round(test_common_words(q1, q2) / test_total_words(q1, q2), 2))
+
+    # bow feature for q1
+    q1_bow = cv.transform([q1]).toarray()
+
+    # bow feature for q2
+    q2_bow = cv.transform([q2]).toarray()
+
+    return np.hstack((np.array(input_query).reshape(1, 7), q1_bow, q2_bow))
